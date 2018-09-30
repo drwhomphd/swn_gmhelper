cf9ef421b4c546ff4ee955f1ef32b78a6474f329
Nathaniel Husted <nhusted@gmail.com>
2018-09-30 19:25:39 -0400

    Initial world generation tests.

--- world_generation.py
+++ world_generation.py
@@ -2,8 +2,8 @@ import gmutils
 
 print("Atmosphere Generation")
 atmospheregen = gmutils.table("tables/world_atmosphere.txt", "Atmosphere")
-atmospheregen.return_result()
+print(atmospheregen.return_result())
 
 print("World Tags Generation")
 worldtaggen = gmutils.table("tables/world_tags.txt", "World Tags")
-worldtaggen.return_result()
+print(worldtaggen.return_result())
