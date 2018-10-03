9aa461674f900fd8a59f9cf75a7b08a4e057ef3a
Nathaniel Husted <nhusted@gmail.com>
2018-10-03 14:04:09 -0400

    Added license information.

--- world_generation.py
+++ world_generation.py
@@ -1,3 +1,5 @@
+# This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
 import gmutils
 
 print("\033[1mDetermine Atmosphere\033[0m")
